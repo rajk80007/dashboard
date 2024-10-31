58e4d093d5f5efc1ce34f588c1792322ab98e0b5
raj <rajk80007@gmail.com>
2024-11-01 01:41:30 +0530

    sdafsdf

--- dashboard-frontend/src/main.jsx
+++ dashboard-frontend/src/main.jsx
@@ -13,7 +13,7 @@ const router  = createBrowserRouter(
   createRoutesFromElements(
     <>
     <Route path='/' element={<App />} >
-      <Route index element={<Home />} />
+      <Route path='/' element={<Home />} />
       <Route path='/show' element={<Show />} /> 
       <Route path='/insert' element={<InsertData />} /> 
     </Route>
