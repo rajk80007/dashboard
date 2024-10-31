2a008e6096c836e313a4527eeda488aa19373b80
raj <rajk80007@gmail.com>
2024-10-31 22:37:09 +0530

    debugging

--- dashboard-frontend/src/components/Sidebar.jsx
+++ dashboard-frontend/src/components/Sidebar.jsx
@@ -28,23 +28,23 @@ const Sidebar = () => {
                 onClick={() => setIsOpen(!isOpen) && this.classList.toggle('left-64')}
                  className='lg:hidden z-50 w-12 h-12 absolute top-24 left-64 duration-300 text-[#ffffff] hover:text-[#413a3a] cursor-pointer hover:shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] p-2 rounded ' />)
             :(<ArrowRightEndOnRectangleIcon
-                onClick={() => setIsOpen(!isOpen)} className='lg:hidden z-50 w-12 h-12 absolute fixed top-24 left-0 text-[#6e6161] hover:text-[#413a3a] cursor-pointer hover:shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] p-2 rounded ' />)
+                onClick={() => setIsOpen(!isOpen)} className='lg:hidden z-50 w-12 h-12 absolute top-24 left-0 text-[#6e6161] hover:text-[#413a3a] cursor-pointer hover:shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] p-2 rounded ' />)
         }
             <div className={`w-[300px] absolute lg:top-[76px] top-[87px] z-10 bg-[#613e59] text-white h-screen py-5 ${isOpen ? 'ml-0' : 'ml-[-300px]'} duration-500 transition-all shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)]`}>
                 <h2 className='text-xl font-bold text-center'>Sidebar</h2>
                 <ul className='flex flex-col gap-3 px-5 mt-5 text-lg text-center'>
                     <li className='hover:text-[#413a3a] cursor-pointer hover:bg-[#f7d2cf] p-2 rounded '>
-                       <Link to='/' onClick={() => setIsOpen(!isOpen)}>
+                       <Link to='/' >
                         Analytics
                        </Link>
                     </li>
                     <li className='hover:text-[#413a3a] cursor-pointer hover:bg-[#f7d2cf] p-2 rounded '>
-                       <Link to='/insert' onClick={() => setIsOpen(!isOpen)}>
+                       <Link to='/insert'>
                         Insert Data
                        </Link>
                     </li>
                     <li className='hover:text-[#413a3a] cursor-pointer hover:bg-[#f7d2cf] p-2 rounded '>
-                       <Link to='/show' onClick={() => setIsOpen(!isOpen)}>
+                       <Link to='/show' >
                         show Data
                        </Link>
                     </li>
